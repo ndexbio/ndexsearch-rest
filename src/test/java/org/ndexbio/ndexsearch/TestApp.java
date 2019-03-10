a5b0aadf2918d28ed6eb60adf5404fb246c20a38
Chris Churas <churas.camera@gmail.com>
2019-03-10 18:47:06 -0500

    initial revision

---
+++ src/test/java/org/ndexbio/ndexsearch/TestApp.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ndexbio.ndexsearch;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author churas
+ */
+public class TestApp {
+    
+    @Test
+    public void testGenerateExampleConfiguration(){
+        String res = App.generateExampleConfiguration();
+        assertTrue(res.contains("# Example configuration file for Search service"));
+        assertTrue(res.contains("NDExSearch/1.0"));
+    }
+    
+    @Test
+    public void testGenerateExampleSourceResults() throws Exception {
+        String res = App.generateExampleSourceResults();
+        assertTrue(res.contains("This is a description of enrichment source"));
+        
+    }
+    
+    @Test
+    public void testExampleModes(){
+        String[] args = {"--mode", App.EXAMPLE_CONF_MODE};
+        App.main(args);
+        String[] oargs = {"--mode", App.EXAMPLE_SOURCERES_MODE};
+        App.main(oargs);
+    }
+}
