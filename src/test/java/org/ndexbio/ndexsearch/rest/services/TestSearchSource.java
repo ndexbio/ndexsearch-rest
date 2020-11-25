2cdff988f53ec98f7f767cde895e7150fa44ced4
Chris Churas <churas.camera@gmail.com>
2020-11-24 17:07:50 -0800

    added tests for getSearchObjects endpoint

--- src/test/java/org/ndexbio/ndexsearch/rest/services/TestSearchSource.java
+++ src/test/java/org/ndexbio/ndexsearch/rest/services/TestSearchSource.java
@@ -6,8 +6,12 @@
 package org.ndexbio.ndexsearch.rest.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
@@ -16,15 +20,18 @@ import org.jboss.resteasy.core.Dispatcher;
 import org.jboss.resteasy.mock.MockDispatcherFactory;
 import org.jboss.resteasy.mock.MockHttpRequest;
 import org.jboss.resteasy.mock.MockHttpResponse;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.ndexbio.enrichment.rest.model.DatabaseResults;
 import org.ndexbio.enrichment.rest.model.ErrorResponse;
+import org.ndexbio.interactomesearch.object.InteractomeRefNetworkEntry;
 import org.ndexbio.ndexsearch.App;
 import org.ndexbio.ndexsearch.rest.engine.SearchEngine;
 import org.ndexbio.ndexsearch.rest.exceptions.SearchException;
+import org.ndexbio.ndexsearch.rest.model.SourceResult;
 import org.ndexbio.ndexsearch.rest.model.SourceResults;
 
 /**
@@ -37,17 +44,26 @@ public class TestSearchSource {
     @Rule
     public TemporaryFolder _folder= new TemporaryFolder();
     
+    /**
+     * Little helper method to write out a configuration file and set it as
+     * config file for Configuration object
+     * @param tempDir
+     * @throws Exception 
+     */
+    protected void reloadConfiguration(File tempDir) throws Exception {
+        File confFile = new File(tempDir.getAbsolutePath() + File.separator + "my.conf");
+        FileWriter fw = new FileWriter(confFile);
+        fw.write(App.generateExampleConfiguration());
+        fw.flush();
+        fw.close();
+        Configuration.setAlternateConfigurationFile(confFile.getAbsolutePath());
+    }
+    
     @Test
     public void testGetSourceResultsNoSearchEngine() throws Exception {
         File tempDir = _folder.newFolder();
         try {
-            File confFile = new File(tempDir.getAbsolutePath() + File.separator + "my.conf");
-            FileWriter fw = new FileWriter(confFile);
-            fw.write(App.generateExampleConfiguration());
-            fw.flush();
-            fw.close();
-            Configuration.setAlternateConfigurationFile(confFile.getAbsolutePath());
-            Configuration.reloadConfiguration();
+            reloadConfiguration(tempDir);
             Configuration.getInstance().setSearchEngine(null);
 
             Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
@@ -72,13 +88,7 @@ public class TestSearchSource {
     public void testGetSourceResultsReturnsNull() throws Exception {
         File tempDir = _folder.newFolder();
         try {
-            File confFile = new File(tempDir.getAbsolutePath() + File.separator + "my.conf");
-            FileWriter fw = new FileWriter(confFile);
-            fw.write(App.generateExampleConfiguration());
-            fw.flush();
-            fw.close();
-            Configuration.setAlternateConfigurationFile(confFile.getAbsolutePath());
-            Configuration.reloadConfiguration();
+            reloadConfiguration(tempDir);
             
             // create mock search engine that returns null
             SearchEngine mockEngine = createMock(SearchEngine.class);
@@ -109,13 +119,7 @@ public class TestSearchSource {
     public void testGetSourceResultsThrowsException() throws Exception {
         File tempDir = _folder.newFolder();
         try {
-            File confFile = new File(tempDir.getAbsolutePath() + File.separator + "my.conf");
-            FileWriter fw = new FileWriter(confFile);
-            fw.write(App.generateExampleConfiguration());
-            fw.flush();
-            fw.close();
-            Configuration.setAlternateConfigurationFile(confFile.getAbsolutePath());
-            Configuration.reloadConfiguration();
+            reloadConfiguration(tempDir);
             
             // create mock search engine that throws exception
             SearchEngine mockEngine = createMock(SearchEngine.class);
@@ -146,13 +150,7 @@ public class TestSearchSource {
     public void testGetSourceResultsSuccess() throws Exception {
         File tempDir = _folder.newFolder();
         try {
-            File confFile = new File(tempDir.getAbsolutePath() + File.separator + "my.conf");
-            FileWriter fw = new FileWriter(confFile);
-            fw.write(App.generateExampleConfiguration());
-            fw.flush();
-            fw.close();
-            Configuration.setAlternateConfigurationFile(confFile.getAbsolutePath());
-            Configuration.reloadConfiguration();
+            reloadConfiguration(tempDir);
        
             // create mock search engine that returns null
             SearchEngine mockEngine = createMock(SearchEngine.class);
@@ -177,6 +175,265 @@ public class TestSearchSource {
         } finally {
             _folder.delete();
             Configuration.getInstance().setSearchEngine(null);
+        }
+    }
+	
+	@Test
+    public void testGetSourceObjectsNoSearchEngine() throws Exception {
+        File tempDir = _folder.newFolder();
+        try {
+            reloadConfiguration(tempDir);
+            Configuration.getInstance().setSearchEngine(null);
+
+            Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+            dispatcher.getRegistry().addSingletonResource(new SearchSource());
+            String uuid = UUID.randomUUID().toString();
+            MockHttpRequest request = MockHttpRequest.get(URIHelper.removeDuplicateSlashes(Configuration.V_ONE_PATH + "/" +
+                                                          SearchSource.SOURCE_PATH + "/" + uuid));
+            MockHttpResponse response = new MockHttpResponse();
+            dispatcher.invoke(request, response);
+            assertEquals(500, response.getStatus());
+            ObjectMapper mapper = new ObjectMapper();
+            ErrorResponse er = mapper.readValue(response.getOutput(),
+                    ErrorResponse.class);
+            assertEquals("Configuration error", er.getMessage());
+            assertTrue(er.getDescription().contains("SearchEngine is null, which is"));
+        } finally {
+            _folder.delete();
+        }
+    }
+    
+    @Test
+    public void testGetSourceObjectsNoSourceResults() throws Exception {
+        File tempDir = _folder.newFolder();
+        try {
+            reloadConfiguration(tempDir);
+            
+             // create mock search engine that returns null
+            SearchEngine mockEngine = createMock(SearchEngine.class);
+            SourceResults sr = new SourceResults();
+           
+            expect(mockEngine.getSourceResults()).andReturn(sr);
+            replay(mockEngine);
+            Configuration.getInstance().setSearchEngine(mockEngine);
+
+            Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+            dispatcher.getRegistry().addSingletonResource(new SearchSource());
+            String uuid = UUID.randomUUID().toString();
+            MockHttpRequest request = MockHttpRequest.get(URIHelper.removeDuplicateSlashes(Configuration.V_ONE_PATH + "/" +
+                                                          SearchSource.SOURCE_PATH + "/" + uuid));
+            MockHttpResponse response = new MockHttpResponse();
+            dispatcher.invoke(request, response);
+            assertEquals(500, response.getStatus());
+            ObjectMapper mapper = new ObjectMapper();
+            ErrorResponse er = mapper.readValue(response.getOutput(),
+                    ErrorResponse.class);
+            assertEquals("Error querying for source information", er.getMessage());
+            assertNull(er.getDescription());
+        } finally {
+            _folder.delete();
+        }
+    }
+    
+    @Test
+    public void testGetSourceObjectsNoMatchingService() throws Exception {
+        File tempDir = _folder.newFolder();
+        try {
+            reloadConfiguration(tempDir);
+            
+             // create mock search engine that returns null
+            SearchEngine mockEngine = createMock(SearchEngine.class);
+            SourceResults sr = new SourceResults();
+            List<SourceResult> sRes = new ArrayList<>();
+            SourceResult srOne = new SourceResult();
+            srOne.setName("notmatching");
+            sRes.add(srOne);
+            sr.setResults(sRes);
+           
+            expect(mockEngine.getSourceResults()).andReturn(sr);
+            replay(mockEngine);
+            Configuration.getInstance().setSearchEngine(mockEngine);
+
+            Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+            dispatcher.getRegistry().addSingletonResource(new SearchSource());
+            String uuid = UUID.randomUUID().toString();
+            MockHttpRequest request = MockHttpRequest.get(URIHelper.removeDuplicateSlashes(Configuration.V_ONE_PATH + "/" +
+                                                          SearchSource.SOURCE_PATH + "/" + uuid));
+            MockHttpResponse response = new MockHttpResponse();
+            dispatcher.invoke(request, response);
+            assertEquals(500, response.getStatus());
+            ObjectMapper mapper = new ObjectMapper();
+            ErrorResponse er = mapper.readValue(response.getOutput(),
+                    ErrorResponse.class);
+            assertEquals("Error querying for source information", er.getMessage());
+            assertTrue(er.getDescription().contains("UUID did not match a service"));
+        } finally {
+            _folder.delete();
+        }
+    }
+    
+    @Test
+    public void testGetSourceObjectsServiceNotFound() throws Exception {
+        File tempDir = _folder.newFolder();
+        try {
+            reloadConfiguration(tempDir);
+            
+            String uuid = UUID.randomUUID().toString();
+             // create mock search engine that returns null
+            SearchEngine mockEngine = createMock(SearchEngine.class);
+            SourceResults sr = new SourceResults();
+            List<SourceResult> sRes = new ArrayList<>();
+            SourceResult srOne = new SourceResult();
+            srOne.setName("theservice");
+            srOne.setUuid(uuid);
+            sRes.add(srOne);
+            sr.setResults(sRes);
+           
+            expect(mockEngine.getSourceResults()).andReturn(sr);
+            replay(mockEngine);
+            Configuration.getInstance().setSearchEngine(mockEngine);
+
+            Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+            dispatcher.getRegistry().addSingletonResource(new SearchSource());
+            
+            MockHttpRequest request = MockHttpRequest.get(URIHelper.removeDuplicateSlashes(Configuration.V_ONE_PATH + "/" +
+                                                          SearchSource.SOURCE_PATH + "/" + uuid));
+            MockHttpResponse response = new MockHttpResponse();
+            dispatcher.invoke(request, response);
+            assertEquals(500, response.getStatus());
+            ObjectMapper mapper = new ObjectMapper();
+            ErrorResponse er = mapper.readValue(response.getOutput(),
+                    ErrorResponse.class);
+            assertEquals("Error querying for source information", er.getMessage());
+            assertTrue(er.getDescription().contains("Service not found"));
+        } finally {
+            _folder.delete();
+        }
+    }
+    
+    @Test
+    public void testGetSourceObjectsGetEnrichment() throws Exception {
+        File tempDir = _folder.newFolder();
+        try {
+            reloadConfiguration(tempDir);
+            
+            String uuid = UUID.randomUUID().toString();
+             // create mock search engine that returns null
+            SearchEngine mockEngine = createMock(SearchEngine.class);
+            SourceResults sr = new SourceResults();
+            List<SourceResult> sRes = new ArrayList<>();
+            SourceResult srOne = new SourceResult();
+            srOne.setName(SourceResult.ENRICHMENT_SERVICE);
+            srOne.setUuid(uuid);
+            sRes.add(srOne);
+            sr.setResults(sRes);
+           
+            expect(mockEngine.getSourceResults()).andReturn(sr);
+            
+            DatabaseResults dr = new DatabaseResults();
+            expect(mockEngine.getEnrichmentDatabases()).andReturn(dr);
+            replay(mockEngine);
+            Configuration.getInstance().setSearchEngine(mockEngine);
+
+            Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+            dispatcher.getRegistry().addSingletonResource(new SearchSource());
+            
+            MockHttpRequest request = MockHttpRequest.get(URIHelper.removeDuplicateSlashes(Configuration.V_ONE_PATH + "/" +
+                                                          SearchSource.SOURCE_PATH + "/" + uuid));
+            MockHttpResponse response = new MockHttpResponse();
+            dispatcher.invoke(request, response);
+            assertEquals(200, response.getStatus());
+            ObjectMapper mapper = new ObjectMapper();
+            DatabaseResults res = mapper.readValue(response.getOutput(),
+                    DatabaseResults.class);
+            assertNotNull(res);
+            
+        } finally {
+            _folder.delete();
+        }
+    }
+    
+    @Test
+    public void testGetSourceObjectsGetPPI() throws Exception {
+        File tempDir = _folder.newFolder();
+        try {
+            reloadConfiguration(tempDir);
+            
+            String uuid = UUID.randomUUID().toString();
+             // create mock search engine that returns null
+            SearchEngine mockEngine = createMock(SearchEngine.class);
+            SourceResults sr = new SourceResults();
+            List<SourceResult> sRes = new ArrayList<>();
+            SourceResult srOne = new SourceResult();
+            srOne.setName(SourceResult.INTERACTOME_PPI_SERVICE);
+            srOne.setUuid(uuid);
+            sRes.add(srOne);
+            sr.setResults(sRes);
+           
+            expect(mockEngine.getSourceResults()).andReturn(sr);
+            List<InteractomeRefNetworkEntry> dr = new ArrayList<>();
+            expect(mockEngine.getInteractomePpiDatabases()).andReturn(dr);
+            replay(mockEngine);
+            Configuration.getInstance().setSearchEngine(mockEngine);
+
+            Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+            dispatcher.getRegistry().addSingletonResource(new SearchSource());
+            
+            MockHttpRequest request = MockHttpRequest.get(URIHelper.removeDuplicateSlashes(Configuration.V_ONE_PATH + "/" +
+                                                          SearchSource.SOURCE_PATH + "/" + uuid));
+            MockHttpResponse response = new MockHttpResponse();
+            dispatcher.invoke(request, response);
+            assertEquals(200, response.getStatus());
+            ObjectMapper mapper = new ObjectMapper();
+            
+            List<InteractomeRefNetworkEntry> res = mapper.readValue(response.getOutput(),
+                    new TypeReference<List<InteractomeRefNetworkEntry>>(){});
+            assertNotNull(res);
+            
+        } finally {
+            _folder.delete();
+        }
+    }
+    
+    @Test
+    public void testGetSourceObjectsGetGeneAssociation() throws Exception {
+        File tempDir = _folder.newFolder();
+        try {
+            reloadConfiguration(tempDir);
+            
+            String uuid = UUID.randomUUID().toString();
+             // create mock search engine that returns null
+            SearchEngine mockEngine = createMock(SearchEngine.class);
+            SourceResults sr = new SourceResults();
+            List<SourceResult> sRes = new ArrayList<>();
+            SourceResult srOne = new SourceResult();
+            srOne.setName(SourceResult.INTERACTOME_GENEASSOCIATION_SERVICE);
+            srOne.setUuid(uuid);
+            sRes.add(srOne);
+            sr.setResults(sRes);
+           
+            expect(mockEngine.getSourceResults()).andReturn(sr);
+            List<InteractomeRefNetworkEntry> dr = new ArrayList<>();
+            expect(mockEngine.getInteractomeGeneAssociationDatabases()).andReturn(dr);
+            replay(mockEngine);
+            Configuration.getInstance().setSearchEngine(mockEngine);
+
+            Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+            dispatcher.getRegistry().addSingletonResource(new SearchSource());
+            
+            MockHttpRequest request = MockHttpRequest.get(URIHelper.removeDuplicateSlashes(Configuration.V_ONE_PATH + "/" +
+                                                          SearchSource.SOURCE_PATH + "/" + uuid));
+            MockHttpResponse response = new MockHttpResponse();
+            dispatcher.invoke(request, response);
+            assertEquals(200, response.getStatus());
+            ObjectMapper mapper = new ObjectMapper();
+            
+            List<InteractomeRefNetworkEntry> res = mapper.readValue(response.getOutput(),
+                    new TypeReference<List<InteractomeRefNetworkEntry>>(){});
+            assertNotNull(res);
+            
+        } finally {
+            _folder.delete();
         }
     }
 }
