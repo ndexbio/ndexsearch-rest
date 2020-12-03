6cc08467aa06ec3014bb44315a11e578272d7a0c
Chris Churas <churas.camera@gmail.com>
2020-12-02 16:40:28 -0800

    switched factory to create SourceEngine objects and passing them to the BasicSearchEngineImpl constructor

--- src/main/java/org/ndexbio/ndexsearch/rest/engine/BasicSearchEngineFactory.java
+++ src/main/java/org/ndexbio/ndexsearch/rest/engine/BasicSearchEngineFactory.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.ndexbio.ndexsearch.rest.engine;
 
-import org.ndexbio.enrichment.rest.client.EnrichmentRestClient;
+import java.util.HashMap;
+import java.util.Map;
 import org.ndexbio.enrichment.rest.client.EnrichmentRestClientImpl;
 import org.ndexbio.interactomesearch.client.InteractomeRestClient;
 import org.ndexbio.ndexsearch.rest.model.SourceResult;
 import org.ndexbio.ndexsearch.rest.model.SourceConfiguration;
 import org.ndexbio.ndexsearch.rest.model.SourceConfigurations;
 import org.ndexbio.ndexsearch.rest.services.Configuration;
+import org.ndexbio.rest.client.NdexRestClient;
 import org.ndexbio.rest.client.NdexRestClientModelAccessLayer;
 
 import org.slf4j.Logger;
@@ -49,24 +46,33 @@ public class BasicSearchEngineFactory {
      * @return 
      */
     public SearchEngine getSearchEngine() throws Exception {
-        EnrichmentRestClient enrichClient = null;
-        InteractomeRestClient interactomeClient_i = null;
-        InteractomeRestClient interactomeClient_a = null;
-        
-        for (SourceConfiguration sc : _sourceConfigurations.getSources()) {
-        	
-        	if (sc.getName().equals(SourceResult.ENRICHMENT_SERVICE)){
-                enrichClient = new EnrichmentRestClientImpl(sc.getEndPoint(), "");
-            } else if (sc.getName().equals(SourceResult.INTERACTOME_PPI_SERVICE)){
-            	interactomeClient_i = new InteractomeRestClient(sc.getEndPoint(), "");
-            } else if (sc.getName().equals(SourceResult.INTERACTOME_GENEASSOCIATION_SERVICE)){
-            	interactomeClient_a = new InteractomeRestClient(sc.getEndPoint(), "");
-            } 
-            
+        Map<String,SourceEngine> sources = new HashMap<>();
+        Map<String, Integer> rankMap = new HashMap<>();
+		rankMap.put(SourceResult.INTERACTOME_PPI_SERVICE, 2);
+		rankMap.put(SourceResult.INTERACTOME_GENEASSOCIATION_SERVICE, 3);
+		
+		for (SourceConfiguration sc : _sourceConfigurations.getSources()) {
+			if (sc.getName().equals(SourceResult.ENRICHMENT_SERVICE)){
+				sources.put(SourceResult.ENRICHMENT_SERVICE,
+						new EnrichmentSourceEngine(new EnrichmentRestClientImpl(sc.getEndPoint(),
+								"")));
+             } else if (sc.getName().equals(SourceResult.INTERACTOME_PPI_SERVICE) || 
+					 sc.getName().equals(SourceResult.INTERACTOME_GENEASSOCIATION_SERVICE)){
+				sources.put(sc.getName(), new InteractomeSourceEngine(sc.getName(),
+						new InteractomeRestClient(sc.getEndPoint(), ""), 2));
+					
+             } else if (sc.getName().equals(SourceResult.KEYWORD_SERVICE)) {
+				 sources.put(sc.getName(),
+						 new KeywordSourceEngine(new NdexRestClientModelAccessLayer(new NdexRestClient(sc.getEndPoint())),
+								 sc.getUuid().toString(), Configuration.getInstance().getUnsetImageURL()));
+			}
+			else {
+				 _logger.error("Unknown source {} skipping", sc.getName());
+             } 
         }
         BasicSearchEngineImpl searcher = new BasicSearchEngineImpl(_dbDir,
-                _taskDir, _sourceConfigurations, _sourcePollingInterval, _keywordclient,
-                enrichClient, interactomeClient_i, interactomeClient_a);
+                _taskDir, _sourceConfigurations,
+				_sourcePollingInterval, sources);
         return searcher;
     }
        
