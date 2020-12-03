5f9f986c9bf174a8f30c147c643c24ae9f887c86
Chris Churas <churas.camera@gmail.com>
2020-12-02 16:38:19 -0800

    extracted standard calls found in BasicSearchEngineImpl to call interactome and enrichment endpoints

--- src/main/java/org/ndexbio/ndexsearch/rest/engine/SourceEngine.java
+++ src/main/java/org/ndexbio/ndexsearch/rest/engine/SourceEngine.java
@@ -1,9 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.ndexbio.ndexsearch.rest.engine;
+
+import java.io.InputStream;
+import org.ndexbio.model.exceptions.NdexException;
+import org.ndexbio.ndexsearch.rest.exceptions.SearchException;
+import org.ndexbio.ndexsearch.rest.model.Query;
+import org.ndexbio.ndexsearch.rest.model.SourceQueryResults;
+import org.ndexbio.ndexsearch.rest.model.SourceResult;
 
 /**
  *
@@ -11,4 +13,17 @@ package org.ndexbio.ndexsearch.rest.engine;
  */
 public interface SourceEngine {
     
+	public SourceQueryResults getSourceQueryResults(final Query query);
+	
+	public void updateSourceResult(SourceResult sRes);
+	
+	public int updateSourceQueryResults(SourceQueryResults sqRes);
+	
+	public void delete(final String id) throws SearchException;
+	
+	public InputStream getOverlaidNetworkAsCXStream(final String id, final String networkId) throws SearchException, NdexException;
+	
+	public Object getDatabases() throws SearchException, NdexException;
+	
+	public void shutdown();
 }
