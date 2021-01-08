fc1e24737bdfc823adf889f9b22fcfcbbc97aca0
Chris Churas <churas.camera@gmail.com>
2021-01-07 17:24:08 -0800

    initial revision

---
+++ src/test/java/org/ndexbio/ndexsearch/rest/engine/TestKeywordSourceEngine.java
@@ -0,0 +1,400 @@
+package org.ndexbio.ndexsearch.rest.engine;
+
+
+import java.util.Arrays;
+import java.util.UUID;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.ndexbio.model.exceptions.NdexException;
+import org.ndexbio.model.object.NetworkSearchResult;
+import org.ndexbio.model.object.network.NetworkSummary;
+import org.ndexbio.ndexsearch.rest.model.Query;
+import org.ndexbio.ndexsearch.rest.model.QueryResults;
+import org.ndexbio.ndexsearch.rest.model.SourceQueryResult;
+import org.ndexbio.ndexsearch.rest.model.SourceQueryResults;
+import org.ndexbio.ndexsearch.rest.model.SourceResult;
+import org.ndexbio.rest.client.NdexRestClientModelAccessLayer;
+
+
+
+/**
+ *
+ * @author churas
+ */
+public class TestKeywordSourceEngine {
+
+	@Rule
+	public TemporaryFolder _folder = new TemporaryFolder();
+	
+	@Test
+	public void testGetSourceQueryResultsQueryThrowsNdexException() throws Exception {
+		NdexRestClientModelAccessLayer mockClient = mock(NdexRestClientModelAccessLayer.class);
+	
+		when(mockClient.findNetworks(any(String.class), eq(null), eq(0), eq(100))).thenThrow(new NdexException("error"));
+		
+		KeywordSourceEngine engine = new KeywordSourceEngine(mockClient, "srcTaskid", "imageurl", 1);
+		Query query = new Query();
+		query.setGeneList(Arrays.asList("gene1"));
+		SourceQueryResults sqr = engine.getSourceQueryResults(query);
+		assertEquals(SourceResult.KEYWORD_SERVICE + " failed : error", sqr.getMessage());
+		assertEquals(100, sqr.getProgress());
+		assertEquals(QueryResults.FAILED_STATUS, sqr.getStatus());
+		verify(mockClient).findNetworks(" gene1", null, 0, 100);
+	}
+	
+	@Test
+	public void testGetSourceQueryResultsQueryFindNetworksReturnsNull() throws Exception {
+		NdexRestClientModelAccessLayer mockClient = mock(NdexRestClientModelAccessLayer.class);
+	
+		when(mockClient.findNetworks(any(String.class), eq(null), eq(0), eq(100))).thenReturn(null);
+		
+		KeywordSourceEngine engine = new KeywordSourceEngine(mockClient, "srcTaskid", "imageurl", 1);
+		Query query = new Query();
+		query.setGeneList(Arrays.asList("gene1"));
+		SourceQueryResults sqr = engine.getSourceQueryResults(query);
+		assertEquals("failed for unknown reason", sqr.getMessage());
+		assertEquals(100, sqr.getProgress());
+		assertEquals(QueryResults.FAILED_STATUS, sqr.getStatus());
+		verify(mockClient).findNetworks(" gene1", null, 0, 100);
+	}
+	
+	@Test
+	public void testGetSourceQueryResultsSuccess() throws Exception {
+		NdexRestClientModelAccessLayer mockClient = mock(NdexRestClientModelAccessLayer.class);
+	
+		NetworkSearchResult nrs = new NetworkSearchResult();
+		NetworkSummary nsOne = new NetworkSummary();
+		nsOne.setName("name1");
+		nsOne.setEdgeCount(1);
+		nsOne.setNodeCount(2);
+		UUID externalIdOne = UUID.randomUUID();
+		nsOne.setExternalId(externalIdOne);
+		
+		NetworkSummary nsTwo = new NetworkSummary();
+		nsTwo.setName("name2");
+		nsTwo.setEdgeCount(3);
+		nsTwo.setNodeCount(4);
+		UUID externalIdTwo = UUID.randomUUID();
+		nsTwo.setExternalId(externalIdTwo);
+		
+		nrs.setNetworks(Arrays.asList(nsOne, nsTwo));
+		
+		when(mockClient.findNetworks(any(String.class), eq(null), eq(0), eq(100))).thenReturn(nrs);
+		
+		KeywordSourceEngine engine = new KeywordSourceEngine(mockClient, "srcTaskid", "imageurl", 1);
+		Query query = new Query();
+		query.setGeneList(Arrays.asList("gene1", "gene2"));
+		SourceQueryResults sqr = engine.getSourceQueryResults(query);
+		assertEquals(null, sqr.getMessage());
+		assertEquals(100, sqr.getProgress());
+		assertEquals(QueryResults.COMPLETE_STATUS, sqr.getStatus());
+		
+		SourceQueryResult two = sqr.getResults().get(1);
+		assertEquals(nsTwo.getName(), two.getDescription());
+		assertEquals(nsTwo.getEdgeCount(), two.getEdges());
+		assertEquals(nsTwo.getNodeCount(), two.getNodes());
+		assertEquals(nsTwo.getExternalId().toString(), two.getNetworkUUID());
+		assertEquals(0, two.getPercentOverlap());
+		assertEquals(1, two.getRank());
+		assertEquals("imageurl", two.getImageURL());
+		
+		verify(mockClient).findNetworks(" gene1 gene2", null, 0, 100);
+	}
+
+	/*
+
+	@Test
+	public void testgetSourceQueryResultsQueryReturnsNull() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		when(mockClient.search(any(List.class))).thenReturn(null);
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		Query query = new Query();
+		query.setGeneList(Arrays.asList("gene1"));
+		SourceQueryResults sqr = engine.getSourceQueryResults(query);
+		assertEquals("sourceName failed for unknown reason", sqr.getMessage());
+		assertEquals(100, sqr.getProgress());
+		assertEquals(QueryResults.FAILED_STATUS, sqr.getStatus());
+		verify(mockClient).search(any(List.class));
+	}
+	
+	@Test
+	public void testgetSourceQueryResultsSuccess() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		UUID resUUID = UUID.randomUUID();
+		when(mockClient.search(any(List.class))).thenReturn(resUUID);
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		Query query = new Query();
+		query.setGeneList(Arrays.asList("gene1"));
+		SourceQueryResults sqr = engine.getSourceQueryResults(query);
+		assertEquals(null, sqr.getMessage());
+		assertEquals(0, sqr.getProgress());
+		assertEquals(QueryResults.SUBMITTED_STATUS, sqr.getStatus());
+		assertEquals(resUUID.toString(), sqr.getSourceTaskId());
+		verify(mockClient).search(any(List.class));
+	}
+	
+	@Test
+	public void testUpdateSourceResultWhereClientThrowsException() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		when(mockClient.getDatabase()).thenThrow(new NdexException("error"));
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		SourceResult sRes = new SourceResult();
+		sRes.setStatus("ok");
+		sRes.setNumberOfNetworks(10);
+		engine.updateSourceResult(sRes);
+		assertEquals("error", sRes.getStatus());
+		assertEquals(10, sRes.getNumberOfNetworks());
+		verify(mockClient).getDatabase();
+	}
+	
+	@Test
+	public void testUpdateSourceResultNullResults() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		when(mockClient.getDatabase()).thenReturn(null);
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		SourceResult sRes = new SourceResult();
+		
+		engine.updateSourceResult(sRes);
+		assertNull(sRes.getDatabases());
+		assertEquals("error", sRes.getStatus());
+		assertEquals("1.0", sRes.getVersion());
+		assertEquals(0, sRes.getNumberOfNetworks());
+		verify(mockClient).getDatabase();
+	}
+	
+	@Test
+	public void testUpdateSourceResultSuccess() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		InteractomeRefNetworkEntry e1 = new InteractomeRefNetworkEntry();
+		InteractomeRefNetworkEntry e2 = new InteractomeRefNetworkEntry();
+		
+		when(mockClient.getDatabase()).thenReturn(Arrays.asList(e1, e2));
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		SourceResult sRes = new SourceResult();
+		
+		engine.updateSourceResult(sRes);
+		assertNull(sRes.getDatabases());
+		assertEquals("ok", sRes.getStatus());
+		assertEquals("1.0", sRes.getVersion());
+		assertEquals(2, sRes.getNumberOfNetworks());
+		verify(mockClient).getDatabase();
+	}
+	
+	@Test
+	public void testShutdown() throws NdexException {
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", null, 1);
+		engine.shutdown();
+	}
+
+	@Test
+	public void testGetOverlaidNetworksAsCXStreamSuccess() throws NdexException, SearchException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		ByteArrayInputStream bis = new ByteArrayInputStream(new byte[5]);
+		UUID idUUID = UUID.randomUUID();
+		UUID netUUID = UUID.randomUUID();
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		when(mockClient.getOverlaidNetworkStream(any(UUID.class), any(UUID.class))).thenReturn(bis);
+		InputStream is = engine.getOverlaidNetworkAsCXStream(idUUID.toString(), netUUID.toString());
+		assertNotNull(is);
+		
+		verify(mockClient).getOverlaidNetworkStream(idUUID, netUUID);
+	}
+	
+	@Test
+	public void testUpdateSourceQueryResultsSearchStatusThrowsException() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		
+		when(mockClient.getSearchStatus(any(UUID.class))).thenThrow(new NdexException("error"));
+		
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		
+		SourceQueryResults sqRes = new SourceQueryResults();
+		UUID taskUUID = UUID.randomUUID();
+		sqRes.setSourceTaskId(taskUUID.toString());
+		engine.updateSourceQueryResults(sqRes);
+		
+		assertEquals(null, sqRes.getMessage());
+		assertEquals(0, sqRes.getProgress());
+		assertEquals(null, sqRes.getStatus());
+		assertEquals(0, sqRes.getNumberOfHits());
+		assertEquals(null, sqRes.getResults());
+		assertEquals(0, sqRes.getWallTime());
+		verify(mockClient).getSearchStatus(taskUUID);
+	}
+	
+	@Test
+	public void testUpdateSourceQueryResultsCompleteWhereSearchResultsThrowsException() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		
+		SearchStatus searchStatus = new SearchStatus();
+		searchStatus.setMessage("message");
+		searchStatus.setProgress(100);
+		searchStatus.setStatus(SearchStatus.complete);
+		searchStatus.setWallTime(2);
+		
+		when(mockClient.getSearchStatus(any(UUID.class))).thenReturn(searchStatus);
+		when(mockClient.getSearchResult(any(UUID.class))).thenThrow(new NdexException("This search has no result ready. "
+							+ "Search status: processing"));
+		
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		
+		SourceQueryResults sqRes = new SourceQueryResults();
+		UUID taskUUID = UUID.randomUUID();
+		sqRes.setSourceTaskId(taskUUID.toString());
+		engine.updateSourceQueryResults(sqRes);
+		
+		assertEquals(searchStatus.getMessage(), sqRes.getMessage());
+		assertEquals(searchStatus.getProgress(), sqRes.getProgress());
+		assertEquals(searchStatus.getStatus(), sqRes.getStatus());
+		assertEquals(0, sqRes.getNumberOfHits());
+		assertEquals(null, sqRes.getResults());
+		assertEquals(searchStatus.getWallTime(), sqRes.getWallTime());
+
+		verify(mockClient).getSearchStatus(taskUUID);
+		verify(mockClient).getSearchResult(taskUUID);
+	}
+	
+	@Test
+	public void testUpdateSourceQueryResultsCompleteWhereResultsIsNull() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		
+		SearchStatus searchStatus = new SearchStatus();
+		searchStatus.setMessage("message");
+		searchStatus.setProgress(100);
+		searchStatus.setStatus(SearchStatus.complete);
+		searchStatus.setWallTime(2);
+		
+		when(mockClient.getSearchStatus(any(UUID.class))).thenReturn(searchStatus);
+		when(mockClient.getSearchResult(any(UUID.class))).thenReturn(null);
+		
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		
+		SourceQueryResults sqRes = new SourceQueryResults();
+		UUID taskUUID = UUID.randomUUID();
+		sqRes.setSourceTaskId(taskUUID.toString());
+		engine.updateSourceQueryResults(sqRes);
+		
+		assertEquals(searchStatus.getMessage(), sqRes.getMessage());
+		assertEquals(searchStatus.getProgress(), sqRes.getProgress());
+		assertEquals(searchStatus.getStatus(), sqRes.getStatus());
+		assertEquals(0, sqRes.getNumberOfHits());
+		assertEquals(0, sqRes.getResults().size());
+		assertEquals(searchStatus.getWallTime(), sqRes.getWallTime());
+
+		verify(mockClient).getSearchStatus(taskUUID);
+		verify(mockClient).getSearchResult(taskUUID);
+	}
+	
+	@Test
+	public void testUpdateSourceQueryResultsInComplete() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		
+		SearchStatus searchStatus = new SearchStatus();
+		searchStatus.setMessage("message");
+		searchStatus.setProgress(50);
+		searchStatus.setStatus(SearchStatus.processing);
+		searchStatus.setWallTime(2);
+		
+		when(mockClient.getSearchStatus(any(UUID.class))).thenReturn(searchStatus);
+		
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		
+		SourceQueryResults sqRes = new SourceQueryResults();
+		UUID taskUUID = UUID.randomUUID();
+		sqRes.setSourceTaskId(taskUUID.toString());
+		engine.updateSourceQueryResults(sqRes);
+		
+		assertEquals(searchStatus.getMessage(), sqRes.getMessage());
+		assertEquals(searchStatus.getProgress(), sqRes.getProgress());
+		assertEquals(searchStatus.getStatus(), sqRes.getStatus());
+		assertEquals(0, sqRes.getNumberOfHits());
+		assertEquals(0, sqRes.getResults().size());
+		assertEquals(searchStatus.getWallTime(), sqRes.getWallTime());
+		verify(mockClient).getSearchStatus(taskUUID);
+	}
+	
+	@Test
+	public void testUpdateSourceQueryResultsCompleteWithResults() throws NdexException {
+		InteractomeRestClient mockClient = mock(InteractomeRestClient.class);
+		
+		SearchStatus searchStatus = new SearchStatus();
+		searchStatus.setMessage("message");
+		searchStatus.setProgress(100);
+		searchStatus.setStatus(SearchStatus.complete);
+		searchStatus.setWallTime(2);
+		
+		when(mockClient.getSearchStatus(any(UUID.class))).thenReturn(searchStatus);
+		
+		InteractomeSearchResult isrOne = new InteractomeSearchResult();
+		isrOne.setDescription("description1");
+		isrOne.setEdgeCount(1);
+		Set<String> hitGenesOne = new TreeSet<>();
+		hitGenesOne.add("g1");
+		isrOne.setHitGenes(hitGenesOne);
+		isrOne.setNetworkUUID("netid");
+		isrOne.setNodeCount(2);
+		isrOne.setPercentOverlap(3);
+		isrOne.setImageURL("imageurl1");
+		isrOne.setRank(4);
+		isrOne.setDetails(null);
+		
+		InteractomeSearchResult isrTwo = new InteractomeSearchResult();
+		isrOne.setDescription("description2");
+		isrOne.setEdgeCount(5);
+		Set<String> hitGenesTwo = new TreeSet<>();
+		hitGenesOne.add("g2");
+		isrOne.setHitGenes(hitGenesTwo);
+		isrOne.setNetworkUUID("netid2");
+		isrOne.setNodeCount(6);
+		isrOne.setPercentOverlap(7);
+		isrOne.setImageURL("imageurl2");
+		isrOne.setRank(8);
+		isrOne.setDetails(new HashMap<>());
+		
+		when(mockClient.getSearchResult(any(UUID.class))).thenReturn(Arrays.asList(isrOne, isrTwo));
+		
+		InteractomeSourceEngine engine = new InteractomeSourceEngine("sourceName", mockClient, 1);
+		
+		SourceQueryResults sqRes = new SourceQueryResults();
+		UUID taskUUID = UUID.randomUUID();
+		sqRes.setSourceTaskId(taskUUID.toString());
+		engine.updateSourceQueryResults(sqRes);
+		
+		assertEquals(searchStatus.getMessage(), sqRes.getMessage());
+		assertEquals(searchStatus.getProgress(), sqRes.getProgress());
+		assertEquals(searchStatus.getStatus(), sqRes.getStatus());
+		assertEquals(2, sqRes.getNumberOfHits());
+		assertEquals(2, sqRes.getResults().size());
+		assertEquals(searchStatus.getWallTime(), sqRes.getWallTime());
+
+		SourceQueryResult resOne = sqRes.getResults().get(0);
+		assertEquals(isrOne.getDescription(), resOne.getDescription());
+		assertEquals(isrOne.getDetails(), resOne.getDetails());
+		assertEquals(isrOne.getEdgeCount(), resOne.getEdges());
+		assertEquals(isrOne.getHitGenes(), resOne.getHitGenes());
+		assertEquals(isrOne.getImageURL(), resOne.getImageURL());
+		assertEquals(isrOne.getNetworkUUID(), resOne.getNetworkUUID());
+		assertEquals(isrOne.getNodeCount(), resOne.getNodes());
+		assertEquals(isrOne.getPercentOverlap(), resOne.getPercentOverlap());
+		assertEquals(isrOne.getRank(), resOne.getRank());
+		
+		SourceQueryResult resTwo = sqRes.getResults().get(1);
+		assertEquals(isrTwo.getDescription(), resTwo.getDescription());
+		assertEquals(isrTwo.getDetails(), resTwo.getDetails());
+		assertEquals(isrTwo.getEdgeCount(), resTwo.getEdges());
+		assertEquals(isrTwo.getHitGenes(), resTwo.getHitGenes());
+		assertEquals(isrTwo.getImageURL(), resTwo.getImageURL());
+		assertEquals(isrTwo.getNetworkUUID(), resTwo.getNetworkUUID());
+		assertEquals(isrTwo.getNodeCount(), resTwo.getNodes());
+		assertEquals(isrTwo.getPercentOverlap(), resTwo.getPercentOverlap());
+		assertEquals(isrTwo.getRank(), resTwo.getRank());
+		
+		verify(mockClient).getSearchStatus(taskUUID);
+		verify(mockClient).getSearchResult(taskUUID);
+	}
+	*/
+}
