adeb92ad38920a94bf44a99cf5cfabedb99382f7
Chris Churas <churas.camera@gmail.com>
2021-11-08 16:30:13 -0800

    removed space

--- src/main/java/org/ndexbio/ndexsearch/rest/CorsFilter.java
+++ src/main/java/org/ndexbio/ndexsearch/rest/CorsFilter.java
@@ -38,7 +38,6 @@ public class CorsFilter implements Filter {
             throws IOException, ServletException {
  
         HttpServletRequest request = (HttpServletRequest) servletRequest;
- 
         // Authorize (allow) all domains to consume the content
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
