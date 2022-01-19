2ae710212f075a5f292f971e909db17a66ea3a5d
Chris Churas <churas.camera@gmail.com>
2022-01-18 20:26:43 -0800

    added mockmutationfrequency service

--- src/main/java/org/ndexbio/ndexsearch/rest/SearchApplication.java
+++ src/main/java/org/ndexbio/ndexsearch/rest/SearchApplication.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.ndexbio.ndexsearch.rest.services.MockMutationFrequency;
 import org.ndexbio.ndexsearch.rest.services.Search;
 import org.ndexbio.ndexsearch.rest.services.SearchSource;
 import org.ndexbio.ndexsearch.rest.services.Status;
@@ -29,6 +30,7 @@ public class SearchApplication extends Application {
                 SearchSource.class,
                 Status.class,
                 OpenApiResource.class,
+				MockMutationFrequency.class,
                 AcceptHeaderOpenApiResource.class).collect(Collectors.toSet());
     }
 }
