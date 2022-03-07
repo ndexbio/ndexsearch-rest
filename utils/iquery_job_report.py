343d19743a4cb5a0a5f6243ee143613eeefb4669
rpillich <rpillich@ucsd.edu>
2022-03-07 15:46:42 -0800

    Added filtering to remove jobs that use example queries

--- utils/iquery_job_report.py
+++ utils/iquery_job_report.py
@@ -125,20 +125,35 @@ def _get_jobs_run_from_tasks(tasks_dir):
         jobdate = datetime.fromtimestamp(qres['startTime'] / 1000.0)
         status = qres['status']
         # walltime = qres['wallTime']
+        
+        # Example gene lists to compare to query gene list;
+        hypoxia = ['ADM', 'ADORA2B', 'AK4', 'AKAP12', 'ALDOA', 'ALDOB', 'ALDOC', 'AMPD3', 'ANGPTL4', 'ANKZF1', 'ANXA2', 'ATF3', 'ATP7A', 'B3GALT6', 'B4GALNT2', 'BCAN', 'BCL2', 'BGN', 'BHLHE40', 'BNIP3L', 'BRS3', 'BTG1', 'CA12', 'CASP6', 'CAV1', 'CCNG2', 'CCRN4L', 'CDKN1A', 'CDKN1B', 'CDKN1C', 'CHST2', 'CHST3', 'CITED2', 'COL5A1', 'CP', 'CSRP2', 'CTGF', 'CXCR4', 'CXCR7', 'CYR61', 'DCN', 'DDIT3', 'DDIT4', 'DPYSL4', 'DTNA', 'DUSP1', 'EDN2', 'EFNA1', 'EFNA3', 'EGFR', 'ENO1', 'ENO2', 'ENO3', 'ERO1L', 'ERRFI1', 'ETS1', 'EXT1', 'F3', 'FAM162A', 'FBP1', 'FOS', 'FOSL2', 'FOXO3', 'GAA', 'GALK1', 'GAPDH', 'GAPDHS', 'GBE1', 'GCK', 'GCNT2', 'GLRX', 'GPC1', 'GPC3', 'GPC4', 'GPI', 'GRHPR', 'GYS1', 'HAS1', 'HDLBP', 'HEXA', 'HK1', 'HK2', 'HMOX1', 'HOXB9', 'HS3ST1', 'HSPA5', 'IDS', 'IER3', 'IGFBP1', 'IGFBP3', 'IL6', 'ILVBL', 'INHA', 'IRS2', 'ISG20', 'JMJD6', 'JUN', 'KDELR3', 'KDM3A', 'KIF5A', 'KLF6', 'KLF7', 'KLHL24', 'LALBA', 'LARGE', 'LDHA', 'LDHC', 'LOX', 'LXN', 'MAFF', 'MAP3K1', 'MIF', 'MT1E', 'MT2A', 'MXI1', 'MYH9', 'NAGK', 'NCAN', 'NDRG1', 'NDST1', 'NDST2', 'NEDD4L', 'NFIL3', 'NR3C1', 'P4HA1', 'P4HA2', 'PAM', 'PCK1', 'PDGFB', 'PDK1', 'PDK3', 'PFKFB3', 'PFKL', 'PFKP', 'PGAM2', 'PGF', 'PGK1', 'PGM1', 'PGM2', 'PHKG1', 'PIM1', 'PKLR', 'PKP1', 'PLAC8', 'PLAUR', 'PLIN2', 'PNRC1', 'PPARGC1A', 'PPFIA4', 'PPP1R15A', 'PPP1R3C', 'PRDX5', 'PRKCA', 'PRKCDBP', 'PTRF', 'PYGM', 'RBPJ', 'RORA', 'RRAGD', 'S100A4', 'SAP30', 'SCARB1', 'SDC2', 'SDC3', 'SDC4', 'SELENBP1', 'SERPINE1', 'SIAH2', 'SLC25A1', 'SLC2A1', 'SLC2A3', 'SLC2A5', 'SLC37A4', 'SLC6A6', 'SRPX', 'STBD1', 'STC1', 'STC2', 'SULT2B1', 'TES', 'TGFB3', 'TGFBI', 'TGM2', 'TIPARP', 'TKTL1', 'TMEM45A', 'TNFAIP3', 'TPBG', 'TPD52', 'TPI1', 'TPST2', 'UGP2', 'VEGFA', 'VHL', 'VLDLR', 'WISP2', 'WSB1', 'XPNPEP1', 'ZFP36', 'ZNF292']
+        
+        death = ['APAF1', 'BCL2', 'BID', 'BIRC2', 'BIRC3', 'CASP10', 'CASP3', 'CASP6', 'CASP7', 'CFLAR', 'CHUK', 'DFFA', 'DFFB', 'FADD', 'GAS2', 'LMNA', 'MAP3K14', 'NFKB1', 'RELA', 'RIPK1', 'SPTAN1', 'TNFRSF25', 'TNFSF10', 'TRADD', 'TRAF2', 'XIAP']
+        
+        ros = ['ABCC1', 'ATOX1', 'CAT', 'CDKN2D', 'EGLN2', 'ERCC2', 'FES', 'FTL', 'G6PD', 'GCLC', 'GCLM', 'GLRX', 'GLRX2', 'GPX3', 'GPX4', 'GSR', 'HHEX', 'HMOX2', 'IPCEF1', 'JUNB', 'LAMTOR5', 'LSP1', 'MBP', 'MGST1', 'MPO', 'MSRA', 'NDUFA6', 'NDUFB4', 'NDUFS2', 'NQO1', 'OXSR1', 'PDLIM1', 'PFKP', 'PRDX1', 'PRDX2', 'PRDX4', 'PRDX6', 'PRNP', 'PTPA', 'SBNO2', 'SCAF4', 'SELENOS', 'SOD1', 'SOD2', 'SRXN1', 'STK25', 'TXN', 'TXNRD1', 'TXNRD2']
+        
+        coxsackie = ['C2AIL', 'CARF', 'CBX1', 'CISY', 'CPSF6', 'CSK21', 'DDX1', 'DPYL2', 'EIF3A', 'EIF3B', 'EIF3C', 'EIF3D', 'EIF3E', 'EIF3F', 'EIF3G', 'EIF3H', 'EIF3I', 'EIF3K', 'EIF3L', 'EIF3M', 'F120A', 'FA98A', 'FBSP1', 'IF4G1', 'IMA5', 'LAR4B', 'MYCB2', 'NCBP3', 'NUP98', 'PAN2', 'PRC2B', 'PTBP1', 'PURB', 'RAE1L', 'RAVR1', 'REL', 'RING1', 'RTRAF', 'SETD3', 'TM225', 'XRN2', 'YTHD3']
+        
         if 'inputSourceList' in qres:
-            if len(qres['inputSourceList']) == 1 and 'enrichment' in qres['inputSourceList'][0]:
-                cdaps_job = True
+          if len(qres['inputSourceList']) == 1 and 'enrichment' in qres['inputSourceList'][0]:
+            cdaps_job = True
 
         if jobdate.year not in job_rundates_hash:
             job_rundates_hash[jobdate.year] = {}
         if jobdate.month not in job_rundates_hash[jobdate.year]:
             job_rundates_hash[jobdate.year][jobdate.month] = [0, 0, 0]
-        if 'complete' not in status:
+            
+        # Modify query gene list and make comparison with example list
+        query_genes = [item.upper() for item in qres['query']]
+        query_genes.sort()
+        if query_genes != hypoxia and query_genes != death and query_genes != ros and query_genes != coxsackie:
+          if 'complete' not in status:
             tuple_index = 2
-        else:
+          else:
             tuple_index = 0
-        job_rundates_hash[jobdate.year][jobdate.month][tuple_index] += 1
-        if cdaps_job is True:
+          job_rundates_hash[jobdate.year][jobdate.month][tuple_index] += 1
+          if cdaps_job is True:
             job_rundates_hash[jobdate.year][jobdate.month][1] += 1
     return job_rundates_hash
 
