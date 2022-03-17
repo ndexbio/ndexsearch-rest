9deb4ec578017f272ea780a50250605120c85b39
Chris Churas <churas.camera@gmail.com>
2022-03-16 17:04:04 -0700

    updated to send alterationData in query

--- utils/testquery.py
+++ utils/testquery.py
@@ -6,12 +6,26 @@ import requests
 
 query = {"geneList": ["mtor", "tp53", "a"],
          "sourceList": ["enrichment"],
-         "geneAnnotationServices": {
-            "mutation": "https://iquery-cbio-dev.ucsd.edu/integratedsearch/v1/mutationfrequency",
-            "alteration": "http://localhost"
-         }}
+         "alterationData": [
+            {
+              "gene": "TP53",
+              "altered": 4437,
+              "sequenced": 10336,
+              "percentAltered": "43%"
+            },
+            {
+              "gene": "MTOR",
+              "altered": 329,
+              "sequenced": 10336,
+              "percentAltered": "3%"
+            }
+         ]
+        }
 
-res = requests.post('https://iquery-cbio-dev.ucsd.edu/integratedsearch/v1/',
+dev_url = 'https://iquery-cbio-dev.ucsd.edu/integratedsearch/v1/'
+local_url = 'http://localhost:8290/v1/'
+
+res = requests.post(local_url,
                     headers={'ContentType': 'application/json',
                              'Accept': 'application/json'},
                     json=query, verify=True)
